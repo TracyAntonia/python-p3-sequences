54c352017b84b64c3640b02ecabf99051a0cb0c8
TracyAntonia <aantonjuguna@gmail.com>
2023-08-21 21:22:26 +0300

    completed assignments

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,16 @@
 
 def print_fibonacci(length):
     pass
+    if length == 0:
+        fibonacci_sequence = []
+    elif length == 1:
+        fibonacci_sequence = [0]
+    else:
+        fibonacci_sequence =[0, 1] 
+
+        while len(fibonacci_sequence) < length:
+            next_fibonacci = fibonacci_sequence[-1] +fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_fibonacci)
+    
+    print(fibonacci_sequence)
+            
